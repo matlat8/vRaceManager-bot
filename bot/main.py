1ea412b17611ded3142dc0d4c94f08cca5cbb013
Vegetable <mlatham@latham.xyz>
2024-03-22 22:06:34 -0400

    added officalpoints stats embed

--- bot/main.py
+++ bot/main.py
@@ -3,8 +3,10 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import asyncio
 
-import driver
+import driver as driver
+from cogs.stats import Stats
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -12,13 +14,25 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(intents=intents, command_prefix='!')
+bot = commands.Bot(intents=intents, command_prefix='?')
 
 ## Add other commands
+#bot.add_cog(Stats(bot))
 bot.add_command(driver.driver)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
+async def setup(bot):
+    await bot.add_cog(Stats(bot))
+
+#bot.run(TOKEN)
+
+async def setup():
+    bot = commands.Bot(command_prefix='?', intents=intents)
+    await bot.add_cog(Stats(bot))
+    return bot
+
+bot = asyncio.run(setup())
 bot.run(TOKEN)
